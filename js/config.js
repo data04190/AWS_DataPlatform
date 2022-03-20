8f0997d4d0d8db79102a9247f058ccd21644aade
Myokyeong Kim <77683645+data04190@users.noreply.github.com>
2022-03-21 00:19:11 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,10 +2,10 @@ window._config = {
 
     cognito: {
         //userPoolId: 'ap-northeast-2_dbmyeu6mg', // e.g. us-east-2_uXboG5pAb
-        userPoolId : 'ap-northeast-2_VZhDziE3I',
+        userPoolId : 'ap-northeast-2_4D9Vxpt6k',
         region: 'ap-northeast-2', // e.g. us-east-2
 		//clientId: '2cqhcb5a1732gi4337vb26b0en' //is this used anywhere?
-        clientId : '15o64rt735qni5h6agu3abc06u'
+        clientId : '5ijs3ha1o19p7pakoju4r8pgtd'
     },
 
 };
