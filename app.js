29af9816f8d566fc22c1965acb30db0dedc58ce9
Myokyeong Kim <77683645+data04190@users.noreply.github.com>
2022-04-04 20:48:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -37,9 +37,14 @@ AWS.config.update({
     }	    
   })
 });
-
-
 */
+
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
 
 var s3 = new AWS.S3({
   apiVersion: '2006-03-01',
