481882e4f3af82505cd8ede3af028f8b9923461a
Myokyeong Kim <77683645+data04190@users.noreply.github.com>
2022-04-05 19:42:54 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,8 @@ window._config = {
         userPoolId : 'ap-northeast-2_4D9Vxpt6k',
         region: 'ap-northeast-2', // e.g. us-east-2
 		//clientId: '2cqhcb5a1732gi4337vb26b0en' //is this used anywhere?
-        clientId : '5ijs3ha1o19p7pakoju4r8pgtd'
+        clientId : '5ijs3ha1o19p7pakoju4r8pgtd',
+	IdentityPoolId: 'ap-northeast-2:3a5facf1-3a37-41ee-8d05-ed417fd9c0d9',
     },
 
 };
