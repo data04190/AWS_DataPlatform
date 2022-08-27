00344631e1001307284a3f7437846b27e156f8c5
Myo-Kyeong Kim <77683645+data04190@users.noreply.github.com>
2022-08-27 23:51:45 +0900

    20220828 - 코드 정리

--- js/config.js
+++ js/config.js
@@ -1,13 +1,10 @@
 window._config = {
 
-    cognito: {
-        //userPoolId: 'ap-northeast-2_dbmyeu6mg', // e.g. us-east-2_uXboG5pAb
+    cognito: {        
         userPoolId : 'ap-northeast-2_4D9Vxpt6k',
         region: 'ap-northeast-2', // e.g. us-east-2
-		//clientId: '2cqhcb5a1732gi4337vb26b0en' //is this used anywhere?
         clientId : '5ijs3ha1o19p7pakoju4r8pgtd',
-	IdentityPoolId: 'ap-northeast-2:3a5facf1-3a37-41ee-8d05-ed417fd9c0d9',
+		IdentityPoolId: 'ap-northeast-2:3a5facf1-3a37-41ee-8d05-ed417fd9c0d9',
     },
 
 };
-
